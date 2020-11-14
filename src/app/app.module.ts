1d0e0ef92d824d9a0959d87a5ab7f28711fa94e4
Pierre Diot <diot.pierre1@gmail.com>
2020-11-14 19:17:55 +0100

    Initial commit for branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UploadComponent } from './components/upload/upload.component';
-import { HomeComponent } from './components/home/home.component';
-import { GameListComponent } from './components/game-list/game-list.component';
 import { SidePanelComponent } from './components/side-panel/side-panel.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FilterFormComponent } from './components/filter-form/filter-form.component';
 import { MainPanelComponent } from './components/main-panel/main-panel.component';
 import { StatsGameSelectComponent } from './components/stats-game-select/stats-game-select.component';
-import { StatsDisplayComponent } from './components/stats-display/stats-display.component';
 import { StatsLineComponent } from './components/stats-line/stats-line.component';
 import { FoldableBlockComponent } from './components/foldable-block/foldable-block.component';
-import { CompareComponent } from './components/compare/compare.component';
 import { StatsCompareDisplayComponent } from './components/stats-compare-display/stats-compare-display.component';
 import { StatsDisplayBlockComponent } from './components/stats-display-block/stats-display-block.component';
 import { MenuComponent } from './components/menu/menu.component';
@@ -22,6 +18,12 @@ import { GameLineComponent } from './components/game-line/game-line.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { GraphsComponent } from './components/graphs/graphs.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StatsDisplayReworkComponent } from './pages/stats-display-rework/stats-display-rework.component';
+import { CompareComponent } from './pages/compare/compare.component';
+import { GameListComponent } from './pages/game-list/game-list.component';
+import { HomeComponent } from './pages/home/home.component';
+import { StatsDisplayComponent } from './pages/stats-display/stats-display.component';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   declarations: [
@@ -41,7 +43,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     StatsDisplayBlockComponent,
     MenuComponent,
     GameLineComponent,
-    GraphsComponent
+    GraphsComponent,
+    StatsDisplayReworkComponent
   ],
   imports: [
     BrowserModule,
@@ -49,7 +52,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     ReactiveFormsModule,
     BrowserAnimationsModule,
     NgbModule,
-    NgxChartsModule
+    NgxChartsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
